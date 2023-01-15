9d6bddd9662948a2f0c6ee42a74602753e26d559
Lucas24865 <lucasnicolasangel@gmail.com>
2023-01-15 20:27:03 -0300

    Cors updated

--- src/main/java/org/LucasA/Lucas/Portfolio/Back/Controller/CredentialController.java
+++ src/main/java/org/LucasA/Lucas/Portfolio/Back/Controller/CredentialController.java
@@ -5,11 +5,13 @@ import org.LucasA.Lucas.Portfolio.Back.Model.RegisterRequest;
 import org.LucasA.Lucas.Portfolio.Back.Service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-angell.web.app"})
 @RestController
 @RequestMapping("/api/auth")
 public class CredentialController {
--- src/main/java/org/LucasA/Lucas/Portfolio/Back/Controller/PortfolioTextController.java
+++ src/main/java/org/LucasA/Lucas/Portfolio/Back/Controller/PortfolioTextController.java
@@ -6,6 +6,7 @@ import org.LucasA.Lucas.Portfolio.Back.Service.IPortfolioTextService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-angell.web.app"})
 @RestController
 @RequestMapping("/api")
 public class PortfolioTextController {
